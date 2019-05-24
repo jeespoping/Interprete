4c280f4ae4069c134da8fed1267af68fe6174cde
Carlos Prada <carlosprada95@gmail.com>
2019-05-24 00:12:16 -0500

    creando la extension para nuestros archivos '.lopez'

--- interpretador/gui.py
+++ interpretador/gui.py
@@ -26,7 +26,7 @@ class Aplicacion:
         sys.exit()
 
     def guardar(self):
-        nombrearch=fd.asksaveasfilename(initialdir = "/",title = "Guardar como",filetypes = (("txt files","*.txt"),("todos los archivos","*.*")))
+        nombrearch=fd.asksaveasfilename(initialdir = "/",title = "Guardar como",filetypes = (("lopez files","*.lopez"),("todos los archivos","*.*")))
         if nombrearch!='':
             archi1=open(nombrearch, "w", encoding="utf-8")
             archi1.write(self.scrolledtext1.get("1.0", tk.END))
