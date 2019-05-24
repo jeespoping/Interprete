a99511d20690fff58e10404fac02d3bf8394ca27
jeespoping <jeespoping@gmail.com>
2019-05-23 23:58:18 -0500

    actualizando

--- interpretador/ide.py
+++ interpretador/ide.py
@@ -1,67 +1,44 @@
-import comander
-from comander import *
-from pygame.locals import *
-import pygame, sys
+import tkinter as tk
+from tkinter import scrolledtext as st
+import sys
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 
-import time
+class Aplicacion:
+    def __init__(self):
+        self.ventana1=tk.Tk()
+        self.agregar_menu()
+        self.scrolledtext1=st.ScrolledText(self.ventana1, width=80, height=20)
+        self.scrolledtext1.grid(column=0,row=0, padx=10, pady=10)
+        self.ventana1.mainloop()
 
-color = (255,255,255)
+    def agregar_menu(self):
+        menubar1 = tk.Menu(self.ventana1)
+        self.ventana1.config(menu=menubar1)
+        opciones1 = tk.Menu(menubar1, tearoff=0)
+        opciones1.add_command(label="Guardar archivo", command=self.guardar)
+        opciones1.add_command(label="Recuperar archivo", command=self.recuperar)
+        opciones1.add_separator()
+        opciones1.add_command(label="Salir", command=self.salir)
+        menubar1.add_cascade(label="Archivo", menu=opciones1)
 
+    def salir(self):
+        sys.exit()
 
-def colenguaje():
-    palabra = ""
-    puntero = item(70 + (45 * len(palabra)), 220, "puntero.png", "puntero2.png")
-    nodos=list()
-    nodos.append(stack)
-    contador=0
-    pygame.init()
+    def guardar(self):
+        nombrearch=fd.asksaveasfilename(initialdir = "/",title = "Guardar como",filetypes = (("txt files","*.lopez"),("todos los archivos","*.*")))
+        if nombrearch!='':
+            archi1=open(nombrearch, "w", encoding="utf-8")
+            archi1.write(self.scrolledtext1.get("1.0", tk.END))
+            archi1.close()
+            mb.showinfo("Información", "Los datos fueron guardados en el archivo.")
 
-    mifuente = []
-    mitexto = []
-
-    for index in range(len(palabra)):
-        mifuente.append(pygame.font.Font(None, 30))
-        mitexto.append(mifuente[index].render(palabra[index], 2, (200, 60, 80)))
-
-    ventana = pygame.display.set_mode((800,600))
-    salir = False
-    reloj1 = pygame.time.Clock()
-
-    pygame.display.set_caption("IDE COLENGUAJE")
-
-    gol = Cambiar(palabra)
-
-    while salir!=True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-            if event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_8:
-
-                if event.key ==pygame.K_9:
-
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-
-                if event.key == pygame.K_RIGHT:
-
-
-
-
-        reloj1.tick(15)
-        ventana.fill((255,255,255))
-
-        x = 120
-        y = 300
-        for index in range(len(palabra)):
-            ventana.blit(mitexto[index], (x, y))
-            x+=45
-        pygame.display.update()
-
-    pygame.quit()
-
-
-colenguaje()
+    def recuperar(self):
+        nombrearch=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (("txt files","*.lopez"),("todos los archivos","*.*")))
+        if nombrearch!='':
+            archi1=open(nombrearch, "r", encoding="utf-8")
+            contenido=archi1.read()
+            archi1.close()
+            self.scrolledtext1.delete("1.0", tk.END)
+            self.scrolledtext1.insert("1.0", contenido)
+aplicacion1=Aplicacion()
